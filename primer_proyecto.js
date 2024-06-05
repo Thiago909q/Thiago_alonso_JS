062fe7d998bdb9510a2ea9f18c41dff9b96e5c44
thiago alonso <Alonsotiti4@gmail.com>
2024-06-05 12:26:16 -0300

    repositorio v2

--- primer_proyecto.js
+++ primer_proyecto.js
@@ -1,18 +1,26 @@
 let continuar = true;
 
 while (continuar) {
-    let seleccion = prompt("Selecciona un producto: 1:Remera\ 2:Pantalon\ 3:Buzo");
+    let seleccion = prompt("Selecciona un producto: 1: Remera $15000\ 2: Pantalon $10000\ 3: Buzo $30000\ 4: Zapatillas $50000");
 
     if (seleccion === "1") {
         alert("Has seleccionado: Remera. Precio: $15000");
-    } else if (seleccion === "2") {
+    } 
+    
+    else if (seleccion === "2") {
         alert("Has seleccionado: Pantalon. Precio: $10000");
-    } else if (seleccion === "3") {
+    } 
+    
+    else if (seleccion === "3") {
         alert("Has seleccionado: Buzo. Precio: $30000");
-    } else {
-        alert("Selección inválida. Por favor, ingrese un número válido.");
-        continue;
+    } 
+    
+    else if (seleccion === "4") {
+        alert("Has seleccionado: Zapatillas. Precio: $50000");
     }
+    else{
+        alert("Selección inválida. Por favor, ingrese un número válido.");
+        continue;}
 
     continuar = confirm("¿Quieres seleccionar otro producto?");
 }
